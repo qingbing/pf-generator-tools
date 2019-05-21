f48737455d4f3f19e9e62843c1fd35030bbbcc36
qingbing <qingbing@localhost.localdomain>
2019-05-21 05:30:19 -0400

    验证码修改

--- src/Models/LoginForm.php
+++ src/Models/LoginForm.php
@@ -15,6 +15,8 @@ use Gt\Components\UserIdentity;
 
 class LoginForm extends FormModel
 {
+    /* @var string 验证码route */
+    public static $captchaAction = '//gt/login/captcha';
     /* @var string 用户名 */
     public $username;
     /* @var string 密码 */
@@ -35,7 +37,7 @@ class LoginForm extends FormModel
             ['password', 'password', 'allowEmpty' => false],
             ['password', 'authenticate'],
             ['verifyCode', 'string', 'allowEmpty' => false],
-            ['verifyCode', \Captcha::VALIDATOR, 'captchaAction' => '//gt/login/captcha', 'allowEmpty' => false],
+            ['verifyCode', \Captcha::VALIDATOR, 'captchaAction' => self::$captchaAction, 'allowEmpty' => false],
         ];
     }
 
